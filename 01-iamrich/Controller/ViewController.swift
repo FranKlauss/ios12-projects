8baf2870b1e172d3fc6b06f9e8b18296791e8d8d
Fran <klaussgriffin@gmail.com>
2018-08-10 20:04:06 +0200

    Initial Commit

---
+++ 01-iamrich/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Francisco José del Pino Díaz on 8/8/18.
+//  Copyright © 2018 Francisco José del Pino Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hola que tal")
+    }
+
+
+}
+
